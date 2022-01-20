36958ec120ce0db09bb631e616e93f14f149a2fc
eco <spambaconspam@protonmail.com>
2022-01-20 20:50:18 +0100

    working on encrypted dir support

--- core/sync.py
+++ core/sync.py
@@ -223,12 +223,14 @@ class Sync(SyncAlgorithm):
                     info('sync', 'conflict', f"Choosing A: {a.encrypted_path.name}")
                     rename_path = b_path.parent / self.get_conflict_name(b_path.name)
                     b_path.rename(rename_path)
+                    self.add(a)
                     info("sync", "rename", rename_path.name)
 
                 elif d_hash == b.hash:
                     info('sync', 'conflict', f"Choosing B: {b.encrypted_path.name}")
                     rename_path = a_path.parent / self.get_conflict_name(a_path.name)
                     a_path.rename(rename_path)
+                    self.add(b)
                     info("sync", "rename", rename_path.name)
 
                 else:
